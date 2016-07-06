1c8569cd85a7a3c7b1703fdbeb8028dabb49f840
hugo woodhead <hugowoodhead@hotmail.co.uk>
2016-07-06 08:23:12 +0100

    initial commit

---
+++ requestHeader/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var userParser = require('user-agent-parser');
+
+app.get('/', function(req, res){
+  var ip = req.ip;
+  var lan = req.acceptsLanguages();
+  var machine = req.headers['user-agent'];
+  var soft = userParser(machine).os.name + '; ' + userParser(machine).os.version;
+  var data = {
+    ipaddress: ip,
+    langagage: lan[0],
+      software: soft
+  }
+  res.send(data);
+});
+
+app.listen(3000, function(){
+  console.log('listening on 3000');
+});
